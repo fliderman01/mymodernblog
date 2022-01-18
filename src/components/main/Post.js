b859d150bd44e56a356e2579d0a926232e34f2c9
fliderman01 <fliderman01@gmail.com>
2022-01-18 14:34:23 +0400

    added openHeader, fullTxt(3 files modified)

--- src/components/main/Post.js
+++ src/components/main/Post.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../../style.css';
 
-export default function App({ post: { title, text, author, image }, index }) {
+export default function App({
+  props,
+  post: { title, text, author, image },
+  index,
+}) {
   return (
     <>
-      <h2>{title}</h2>
+      <h2 onClick={() => openHeader(index)}>{title}</h2>
       <h4>By: {author}</h4>
       <img src={image} alt="post" width="330px" />
       <p>{text}</p>
--- src/components/main/Posts.js
+++ src/components/main/Posts.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../style.css';
 import data from '../../data.json';
-import Post from './Post'
+import Post from './Post';
 
-export default function App() {
+export default function App(props) {
+  // import data from data.json
   const blogPosts = data.blogPosts;
-  console.log('hi');
+  const [headerClick, setHeaderClick] = useState('');
+
+  const openHeader = (articleNum) => {
+    setHeaderClick(articleNum);
+    // later MyBlog should set headerClick to '' (empty) or false
+    console.log('hi');
+  };
   return (
-    <article>
-      {blogPosts.map((post, index) => (
-        <Post key={index} index={index} post={post} />
-      ))}
-    </article>
+    <>
+      {headerClick ? (
+        <article>
+          <h1>{blogPosts[headClick].title}</h1>
+          <h4>{blogPosts[headClick].author}</h4>
+          <img src={blogPosts[headClick].image} alt="post" />
+          <p>{blogPosts[headClick].fullTxt}</p>
+        </article>
+      ) : (
+        <article>
+          {blogPosts.map((post, index) => (
+            <Post
+              key={index}
+              index={index}
+              post={post}
+              openHeader={openHeader}
+            />
+          ))}
+        </article>
+      )}
+    </>
   );
 }
