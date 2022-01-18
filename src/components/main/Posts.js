47b7c152ef7221ac54361b55b78c57ad95d3df97
fliderman01 <fliderman01@gmail.com>
2022-01-19 00:23:48 +0400

    solved first post issue - 1 file modified

--- src/components/main/Posts.js
+++ src/components/main/Posts.js
@@ -9,76 +9,53 @@ export default function Posts(props) {
   const blogPosts = data.blogPosts;
 
   const [headerClick, setHeaderClick] = useState('');
+  const [showPost, setShowPost] = useState(false);
 
   // opens individual post
   const openHeader = (articleNum) => {
     setHeaderClick(articleNum);
+    setShowPost(true);
   };
   // go back to main page
   const mainPg = () => {
     setHeaderClick('');
+    setShowPost(false);
   };
   return (
     <>
-      {headerClick ?
-      <article className="postArticle">
-        <h1>{blogPosts[headerClick].title}</h1>
-        <br />
-        <h4>By: {blogPosts[headerClick].author}</h4>
-        <img src={blogPosts[headerClick].image} alt="post" />
-        <br />
-        <p className="postParas">{blogPosts[headerClick].text}</p>
-        <br />
-        {blogPosts[headerClick].fullTxt.map((para) => (
-          <>
-            <p key={Math.random() * 999} className="postParas">
-              {para}
-            </p>
-            <br />
-          </>
-        ))}
-        <a href="#top" id="goBack" onClick={mainPg}>
-          go back to main page
-        </a>
-      </article>
-       :
-      <article>
-        {blogPosts.map((post, index) => (
-          <Post key={index} index={index} post={post} openHeader={openHeader} />
-        ))}
-      </article>
-       }
+      {showPost ? (
+        <article className="postArticle">
+          <h1>{blogPosts[headerClick].title}</h1>
+          <br />
+          <h4>By: {blogPosts[headerClick].author}</h4>
+          <img src={blogPosts[headerClick].image} alt="post" />
+          <br />
+          <p className="postParas">{blogPosts[headerClick].text}</p>
+          <br />
+          {blogPosts[headerClick].fullTxt.map((para) => (
+            <>
+              <p key={Math.random() * 999} className="postParas">
+                {para}
+              </p>
+              <br />
+            </>
+          ))}
+          <a href="#top" id="goBack" onClick={mainPg}>
+            go back to main page
+          </a>
+        </article>
+      ) : (
+        <article>
+          {blogPosts.map((post, index) => (
+            <Post
+              key={index}
+              index={index}
+              post={post}
+              openHeader={openHeader}
+            />
+          ))}
+        </article>
+      )}
     </>
   );
 }
-
-// const bodyPosts = () => {
-//   if (headerClick >= 0) {
-//     <article className="postArticle">
-//       <h1>{blogPosts[headerClick].title}</h1>
-//       <br />
-//       <h4>By: {blogPosts[headerClick].author}</h4>
-//       <img src={blogPosts[headerClick].image} alt="post" />
-//       <br />
-//       <p className="postParas">{blogPosts[headerClick].text}</p>
-//       <br />
-//       {blogPosts[headerClick].fullTxt.map((para) => (
-//         <>
-//           <p key={Math.random() * 999} className="postParas">
-//             {para}
-//           </p>
-//           <br />
-//         </>
-//       ))}
-//       <a href="#top" id="goBack" onClick={mainPg}>
-//         go back to main page
-//       </a>
-//     </article>;
-//   } else if (headerClick == '') {
-//     <article>
-//       {blogPosts.map((post, index) => (
-//         <Post key={index} index={index} post={post} openHeader={openHeader} />
-//       ))}
-//     </article>;
-//   }
-// };
