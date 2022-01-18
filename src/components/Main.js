d79782076aeed63987ea80c4d23b3dd67c05bfc2
fliderman01 <fliderman01@gmail.com>
2022-01-18 14:05:56 +0400

    added Posts in Main (4 files modified)

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,42 +1,12 @@
 import React from 'react';
 import '../style.css';
 import './Main.css';
+import Posts from './main/Posts';
 
 export default function Main() {
   return (
     <main>
-      <article>
-        <h2>State of the Art Infrastructure</h2>
-        <h4>By: Tim Burns Lee</h4>
-        <img className='photos' src="https://www.maxpixel.net/static/photo/1x/Building-Structure-Infrastructure-Architecture-2589447.jpg" alt="Infrastructure" width='330px' />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti odio
-          quasi magnam rerum eum, fugit atque ad alias ab, eius cum, veritatis
-          repellat. Atque!
-        </p>
-      </article>
-      <article>
-        <h2>Free, open, simple</h2>
-        <h4>By: Sun Mi</h4>
-        <img className='photos' src="https://www.maxpixel.net/static/photo/1x/Infrastructure-Architecture-Building-Structure-2573390.jpg" alt="Infrastructure" width='330px' />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-          tenetur fuga saepe dolores ullam provident hic natus eaque mollitia
-          quod porro reprehenderit tempora quaerat, assumenda corrupti possimus
-          vero nemo consectetur cum voluptatum repellat iste?
-        </p>
-      </article>
-      <article>
-        <h2>Free, open, simple</h2>
-        <h4>By: Sun Mi</h4>
-        <img className='photos' src="https://www.maxpixel.net/static/photo/1x/Infrastructure-Architecture-Building-Structure-2573390.jpg" alt="Infrastructure" width='330px' />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-          tenetur fuga saepe dolores ullam provident hic natus eaque mollitia
-          quod porro reprehenderit tempora quaerat, assumenda corrupti possimus
-          vero nemo consectetur cum voluptatum repellat iste?
-        </p>
-      </article>
+      <Posts />
     </main>
   );
 }
--- src/components/main/Post.js
+++ src/components/main/Post.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import '../../style.css';
-// import Posts from './components/Posts'
 
-export default function App({ post: { title, body, author, image }, index }) {
+export default function App({ post: { title, text, author, image }, index }) {
   return (
-    <div>
-      <h1>{title}</h1>
-      <h3>Author: {author}</h3>
-      <img src={image} alt="post" />
-      <p>{body}</p>
-    </div>
+    <>
+      <h2>{title}</h2>
+      <h4>By: {author}</h4>
+      <img src={image} alt="post" width="330px" />
+      <p>{text}</p>
+    </>
   );
 }
