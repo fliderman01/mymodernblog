a5950685cb68885f68a2c5af5ab8310555ec1118
fliderman01 <fliderman01@gmail.com>
2022-01-20 12:16:31 +0400

    added Pagination - 4 files modified

--- src/components/main/Posts.js
+++ src/components/main/Posts.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import ReactPaginate from 'react-paginate';
 import '../../style.css';
 import './Posts.css';
 import data from '../../data.json';
@@ -10,6 +11,31 @@ export default function Posts(props) {
 
   const [headerClick, setHeaderClick] = useState('');
   const [showPost, setShowPost] = useState(false);
+  // for Pagination
+  const [posts, setPosts] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [perPage] = useState(3);
+  const [pageCount, setPageCount] = useState(0);
+  // get json data (posts) for pagination
+  const getData = () => {
+    const data = blogPosts;
+    const sliced = data.slice(offset, offset + perPage);
+    const display = sliced.map((post, index) => (
+      <Post key={index} index={index} post={post} openHeader={openHeader} />
+    ));
+    setPosts(display);
+    setPageCount(Math.ceil(data.length / perPage));
+  };
+  // set offset when page number is clicked
+  const pgClick = (e) => {
+    const selected = e.selected;
+    setOffset(selected * perPage);
+  };
+  // activates getData when offset is activated
+  useEffect(() => {
+    getData();
+    scrollTo(0, 560); // on render(page change) scroll to 1st post header
+  }, [offset]);
 
   // opens individual post
   const openHeader = (articleNum) => {
@@ -43,15 +69,18 @@ export default function Posts(props) {
           </a>
         </article>
       ) : (
-        <article>
-          {blogPosts.map((post, index) => (
-            <Post
-              key={index}
-              index={index}
-              post={post}
-              openHeader={openHeader}
-            />
-          ))}
+        <article className="postsArticle">
+          {posts}
+          <ReactPaginate
+            previousLabel={'prev'}
+            nextLabel={'next'}
+            breakLabel={'...'}
+            breakClassName={'break-me'}
+            pageCount={pageCount}
+            marginPagesDisplayed={2}
+            onPageChange={pgClick}
+            containerClassName={'pagination'}
+          />
         </article>
       )}
     </>
